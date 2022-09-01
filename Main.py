77c113140983e6ac33555c97320e106c3b27915d
mithunmithu11 <110010668+mithunmithu11@users.noreply.github.com>
2022-09-01 21:11:10 +0530

    2nd commit

--- Main.py
+++ Main.py
@@ -130,3 +130,9 @@ for i in range(len(operations)):
         result.append(obj.add_at_index(int(data[i][0]), data[i][1]))
     elif operations[i] == "get":
         result.append(obj.get(data[i]))
+    elif operations[i] == "get_previous_next":
+        result.append(obj.get_previous_next(data[i]))
+    elif operations[i] == 'delete_at_index':
+        result.append(obj.delete_at_index(data[i]))
+
+print(result)
